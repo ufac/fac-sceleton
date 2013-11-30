27537ed5f2db69299e5defcbdfd8308b4da6a52e
Tyurin D <fobia3d@gmail.com>
2013-11-30 06:51:10 +0400

    Бутстрап

---
+++ app/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__ . '/lib/fac/loader.php';
+require_once __DIR__. '/lib/Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+
--- public/index.php
+++ public/index.php
@@ -2,5 +2,43 @@
 /**
  * Инициализация веб интерфейса
  */
-var_dump($_SERVER);
 
+
+require_once __DIR__ . '/../app/bootstrap.php';
+
+$app = new \Slim\Slim();
+
+$app->get('/', function () {
+    var_dump($_SERVER);
+});
+
+// POST route
+$app->post(
+    '/post',
+    function () {
+        echo 'This is a POST route';
+    }
+);
+
+// PUT route
+$app->put(
+    '/put',
+    function () {
+        echo 'This is a PUT route';
+    }
+);
+
+// PATCH route
+$app->patch('/patch', function () {
+    echo 'This is a PATCH route';
+});
+
+// DELETE route
+$app->delete(
+    '/delete',
+    function () {
+        echo 'This is a DELETE route';
+    }
+);
+
+$app->run();
